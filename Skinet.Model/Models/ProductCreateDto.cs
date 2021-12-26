c8c9f90e0a012f8ebf89916cca0fe883eeb604b3
fvanderflier <filip.cejkovski@polarcape.com>
2021-12-26 23:04:15 +0100

    Added ProductCreateDto

---
+++ Skinet.Model/Models/ProductCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skinet.Model.Models
+{
+    public class ProductCreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public IFormFile PictureFile { get; set; }
+        public string ProductType { get; set; }
+        public string ProductBrand { get; set; }
+    }
+}
