7bec0b1d2301fcc511cf0a351e88e14c3b0b4350
fvanderflier <filip.cejkovski@polarcape.com>
2021-12-27 14:16:06 +0100

    Changed product overview page and added product details page

---
+++ Skinet.Client/Pages/ProductDetail.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Skinet.Client.Services.Interface;
+using Skinet.Model.Models;
+using System.Threading.Tasks;
+
+namespace Skinet.Client.Pages
+{
+    public partial class ProductDetail
+    {
+        [Parameter]
+        public string ProductId { get; set; }
+        public ProductReadDto Product { get; set; }
+
+        [Inject]
+        public IProductsService ProductsService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Product = await ProductsService.GetProduct(int.Parse(ProductId));
+        }
+    }
+}
